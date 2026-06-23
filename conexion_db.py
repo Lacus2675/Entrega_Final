ddc31d58316481b7a26eebc1a8f6478f07c30267
Lacus2675 <128506373+Lacus2675@users.noreply.github.com>
2026-06-22 21:24:17 -0300

    Add files via upload

--- conexion_db.py
+++ conexion_db.py
@@ -1,4 +1,5 @@
 # conexion_db.py
+"""
 import sqlite3
 from pathlib import Path
 
@@ -21,6 +22,7 @@ def db():
     _connexion = sqlite3.connect(ruta_db)
     _cursor = _connexion.cursor()
     return _cursor
+    """
 """
 def close_db():
     
@@ -33,3 +35,33 @@ def close_db():
  
 """
 
+import sqlite3
+from pathlib import Path
+
+_db_name = None
+_connexion = None
+_cursor = None
+
+def db():
+    
+    global _db_name, _connexion, _cursor , _conn
+    ruta_archivo = Path(__file__).parent / 'db.txt'
+
+    # Si el archivo no existe, lo creamos con el contenido deseado
+    if not ruta_archivo.exists():
+        ruta_archivo.write_text("inventario1.db\n", encoding='utf-8')
+
+    archivo = open(ruta_archivo, 'r', encoding='utf-8')
+    _db_name = archivo.readline().strip()
+    
+    # Construir ruta completa a la base de datos
+    directorio = Path(__file__).parent / 'Bases_De_Datos'
+    # Asegurar que el directorio existe
+    directorio.mkdir(parents=True, exist_ok=True)
+    ruta_db = directorio / _db_name
+
+    _connexion = sqlite3.connect(ruta_db)
+    _cursor = _connexion.cursor()
+    return _cursor
+
+    
---
+++ ejecutar_opcional.bat
@@ -0,0 +1,2 @@
+@echo off
+start /MAX "" python main.py
